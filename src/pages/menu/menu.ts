064d0f066ea44759f2ebf607e77c324d9d70d63c
Cristian Suarez Vera <cristian.suarez.ulpgc@gmail.com>
2018-07-06 23:25:50 +0100

    Create menu page and navigation to dessert

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,25 +1,21 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {DessertPage} from "../dessert/dessert";
 
-/**
- * Generated class for the MenuPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-menu',
   templateUrl: 'menu.html',
 })
 export class MenuPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MenuPage');
+  navigateToDessert() {
+    this.navCtrl.push(DessertPage);
   }
 
+  navigateToFood() {
+    console.log('Food');
+  }
 }
