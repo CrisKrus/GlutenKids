b8efac183f01381e63984cd57823907b79e7adc3
Cristian Suarez Vera <cristian.suarez.ulpgc@gmail.com>
2018-09-06 20:35:24 +0100

    Show non available recipes on grey

--- src/pages/recipe-list/recipe-list.ts
+++ src/pages/recipe-list/recipe-list.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
 import {RecipePage} from "../recipe/recipe";
 import {Recipe} from "../../model/recipe";
+import {UserProvider} from "../../providers/user/user";
+import {ToastProvider} from "../../providers/toast/toast";
 
 @Component({
     selector: 'page-dessert',
@@ -10,13 +12,28 @@ import {Recipe} from "../../model/recipe";
 export class RecipeListPage {
     private recipes: Array<Recipe>;
     private cook: boolean;
+    private userLevel = '';
 
-    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    constructor(public navCtrl: NavController,
+                public navParams: NavParams,
+                private userProvider: UserProvider,
+                private toast: ToastProvider) {
+        userProvider.level.then((level) => {
+            this.setUserLevel(level);
+        });
         this.recipes = navParams.get('recipes');
         this.cook = navParams.get('cook');
     }
 
+    private setUserLevel(level) {
+        this.userLevel = level;
+    }
+
     navigateToRecipe(recipe: Recipe) {
-        this.navCtrl.push(RecipePage, {recipe: recipe});
+        if (recipe.level > this.userLevel) {
+            this.toast.warning('No tienes el nivel necesario: ' + recipe.level)
+        } else {
+            this.navCtrl.push(RecipePage, {recipe: recipe});
+        }
     }
 }
